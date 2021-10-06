79ca48a61165bde39f724b895690363b4170c43c
Yury Frolov <yuri.frolov96@gmail.com>
2021-10-06 12:24:04 +0700

    add atf__slider, tour__day-1-slider

--- src/components/bem-blocks/slider/slider.js
+++ src/components/bem-blocks/slider/slider.js
@@ -1,16 +1,44 @@
+const atfSlider = document.querySelector('.atf__slider');
+const day1Slider = document.querySelector('.tour__day-1-slider');
+
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   centeredSlides: true,
   spaceBetween: 35,
   loop: false,
+  mousewheel: false,
   breakpoints: {
-    600: {
+    550: {
       slidesPerView: 2,
     },
   },
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
+
+const swiperRtl = new Swiper('.swiper-rtl', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 35,
+  loop: false,
+  mousewheel: false,
+  breakpoints: {
+    550: {
+      slidesPerView: 2,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const mQuery = window.matchMedia('(min-width: 1440px)');
+
+if (mQuery.matches) {
+  day1Slider.classList.remove('overflow-v');
+  day1Slider.classList.add('overflow-h');
+}
+
