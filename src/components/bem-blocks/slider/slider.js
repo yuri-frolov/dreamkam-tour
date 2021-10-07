96d8a598da0867779e1834ef05835cfb436e3b2c
Yury Frolov <yuri.frolov96@gmail.com>
2021-10-07 13:07:18 +0700

    create tour__day-3-slider

--- src/components/bem-blocks/slider/slider.js
+++ src/components/bem-blocks/slider/slider.js
@@ -31,20 +31,23 @@ const swiperRtl = new Swiper('.swiper-rtl', {
     prevEl: '.swiper-button-prev',
   },
 });
+const mediaQuery1440 = window.matchMedia('(min-width: 1440px)');
+const mediaQuery1100 = window.matchMedia('(min-width: 1100px)');
 
 const day1Slider = document.querySelector('.tour__day-1-slider');
-const mediaQuery1440 = window.matchMedia('(min-width: 1440px)');
-
 if (mediaQuery1440.matches) {
   day1Slider.classList.remove('overflow-v');
   day1Slider.classList.add('overflow-h');
 }
 
-
 const day2Slider = document.querySelector('.tour__day-2-slider');
-const mediaQuery1100 = window.matchMedia('(min-width: 1100px)');
-
 if (mediaQuery1100.matches) {
   day2Slider.classList.remove('overflow-v');
   day2Slider.classList.add('overflow-h');
+}
+
+const day3Slider = document.querySelector('.tour__day-3-slider');
+if (mediaQuery1440.matches) {
+  day3Slider.classList.remove('overflow-v');
+  day3Slider.classList.add('overflow-h', 'swiper--buttons-black');
 }
