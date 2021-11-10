3230b73707e41d9f4ec08772b8b53b2efaa3effa
Yury Frolov <yuri.frolov96@gmail.com>
2021-11-10 13:58:11 +0700

    add markup & base styles for modal window

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -1,1 +1,15 @@
+/* let body = document.querySelector('body');
+let modal = document.querySelector('.modal');
 
+let modalFooterClose = document.querySelector('.modal-box__footer-close');
+let modalFooterCloseLink = document.querySelector('.modal-box__footer-button');
+
+document.querySelector('.modal-box__close').addEventListener('click', function() {
+  modal.style.display = "none";
+});
+
+window.onclick = function(event) {
+  if (event.target == modal || event.target == modalFooterClose || event.target == modalFooterCloseLink) {
+    modal.style.display = "none";
+  }
+}; */
